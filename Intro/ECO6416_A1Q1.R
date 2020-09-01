42faaba530da77b229ba909bd2b4c3009f336c0a
Lee Morin <Lealand.Morin@ucf.edu>
2020-09-01 14:55:30 -0400

    Example for Assignment 1

--- Intro/ECO6416_A1Q1.R
+++ Intro/ECO6416_A1Q1.R
@@ -11,12 +11,12 @@
 # College of Business Administration
 # University of Central Florida
 #
-# September 16, 2019
+# August 31, 2020
 #
 ##################################################
 #
-# ECO6416_OLS_Demo gives an example of OLS regression
-#   using data imported entered within this script.
+# ECO6416_A1Q1 gives an example of OLS regression
+#   using data entered within this script.
 #
 # Dependencies:
 #   None.
@@ -33,7 +33,7 @@ rm(list=ls(all=TRUE))
 
 # Set working directory.
 # wd_path <- '/path/to/your/folder'
-# wd_path <- '~/Teaching/ECO6416_Fall2019/Module02' # On Windows
+# wd_path <- 'C:/Users/le279259/Desktop/ECO6416_Demos/Module03' # On Windows
 # Not required, since this program does not interact with other files.
 
 # setwd(wd_path)
@@ -48,10 +48,10 @@ rm(list=ls(all=TRUE))
 ##################################################
 
 # Enter the numbers in the dataset.
-income_data  <- c(14, 18, 18, 16, 16, 26, 20, 18, 20, 22)
+income_data  <- c(6, 8, 8, 7, 7, 12, 9, 8, 9, 10)
 agg_pct_data <- c(9, 10, 8, 7, 10, 4, 5, 5, 6, 7)
-agg_data <- data.frame(income = income_data,
-                       agg_pct = agg_pct_data)
+agg_data <- data.frame(income = income_data*3 + 2,
+                       agg_pct = agg_pct_data*2)
 
 # Inspect the data.
 summary(agg_data)
@@ -62,7 +62,8 @@ plot(agg_data[, 'income'],
      main = 'Aggregate Income vs. Pct. in Agriculture',
      xlab = 'Income',
      ylab = 'Pct. in Agriculture',
-     col = 'blue')
+     col = 'blue',
+     pch = 16)
 
 
 
@@ -87,8 +88,31 @@ x_bar = mean(x)
 beta_cov_term <- sum( (y - y_bar)*(x - x_bar) )
 beta_var_term <- sum( (x - x_bar)^2 )
 
-beta_hat <- beta_cov_term/beta_var_term
+# Calculate the slope coefficient.
+beta_1_hat <- beta_cov_term/beta_var_term
+
+# Calculate the intercept.
+beta_0_hat <- y_bar - beta_1_hat*x_bar
+
 # Compare with the output above.
+
+
+
+
+# Calculation of the R-squared.
+TSS <- sum((y - y_bar)^2)
+y_hat <- beta_0_hat + beta_1_hat*x
+# ESS <- sum( (y_hat - y_bar)^2 ) # Not needed.
+RSS <- sum( (y - y_hat)^2 )
+
+R_2 <- 1 - RSS/TSS
+
+R_bar_2 <- 1 - (RSS/(10 - 1 - 1)) / (TSS/(10 - 1))
+
+
+# Prediction:
+beta_0_hat + beta_1_hat*10
+
 
 
 ##################################################
